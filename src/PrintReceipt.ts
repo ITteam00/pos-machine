96416daf539ffc345f76cf2422aa71f96d542fb9
zjiang10 <zjiang10@slb.com>
2024-10-29 17:36:18 +0800

    feat: init calculateQuantity and calculateSubtotal

--- src/PrintReceipt.ts
+++ src/PrintReceipt.ts
@@ -3,3 +3,13 @@ import {loadAllItems, loadPromotions} from './Dependencies'
 export function printReceipt(tags: string[]): string {
   return ''
 }
+
+function calculateQuantity(tags: string[]) {
+  let itemQuantity = new Map<string, number>()
+  return itemQuantity
+}
+
+function calculateSubtotal(tags: string[]) {
+  let itemSubtotal = new Map<string, number>()
+  return itemSubtotal
+}
