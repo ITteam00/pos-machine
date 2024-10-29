523e348e183b5d290887155a14371f6885bb7163
kgaohahaha <15296612347@163.com>
2024-10-29 20:14:55 +0800

    feat: add unit test

--- test/PrintReceipt.test.ts
+++ test/PrintReceipt.test.ts
@@ -1,4 +1,7 @@
-import {printReceipt} from '../src/PrintReceipt'
+import { printReceipt } from '../src/PrintReceipt'
+import { getItemsCount } from '../src/PrintReceipt'
+import { calculateExpense } from '../src/PrintReceipt'
+import { calculateDiscount } from '../src/PrintReceipt'
 
 describe('printReceipt', () => {
   it('should print receipt with promotion when print receipt', () => {
@@ -23,5 +26,92 @@ Discounted prices：7.50(yuan)
 **********************`
 
     expect(printReceipt(tags)).toEqual(expectText)
+  })
+
+  it('ITEM000001 count should be 5 and isProMotion is True', () => {
+    const tags = [
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000003-2.5',
+      'ITEM000005',
+      'ITEM000005-2',
+    ]
+    const itemDictionary = getItemsCount(tags)
+    expect(itemDictionary['ITEM000001'].quantity).toEqual(5)
+    expect(itemDictionary['ITEM000001'].isProMotion).toEqual(true)
+  })
+
+  it('ITEM000001 totalExpense should be 12', () => {
+    const itemDic =
+    {
+      ITEM000001: {
+        barcode: 'ITEM000001',
+        name: 'Sprite',
+        unit: 'bottle',
+        price: 3,
+        isProMotion: true,
+        quantity: 5,
+        totalExpense: 0
+      },
+      ITEM000003: {
+        barcode: 'ITEM000003',
+        name: 'Litchi',
+        unit: 'pound',
+        price: 15,
+        isProMotion: false,
+        quantity: 2.5,
+        totalExpense: 0
+      },
+      ITEM000005: {
+        barcode: 'ITEM000005',
+        name: 'Instant Noodles',
+        unit: 'bag',
+        price: 4.5,
+        isProMotion: true,
+        quantity: 3,
+        totalExpense: 0
+      }
+    }
+    calculateExpense(itemDic)
+    expect(itemDic['ITEM000001'].totalExpense).toEqual(12)
+  })
+
+  
+  it('Discount should be 7.5 and Totalshould be 58.5', () => {
+    const itemDic =
+    {
+      ITEM000001: {
+        barcode: 'ITEM000001',
+        name: 'Sprite',
+        unit: 'bottle',
+        price: 3,
+        isProMotion: true,
+        quantity: 5,
+        totalExpense: 12
+      },
+      ITEM000003: {
+        barcode: 'ITEM000003',
+        name: 'Litchi',
+        unit: 'pound',
+        price: 15,
+        isProMotion: false,
+        quantity: 2.5,
+        totalExpense: 37.5
+      },
+      ITEM000005: {
+        barcode: 'ITEM000005',
+        name: 'Instant Noodles',
+        unit: 'bag',
+        price: 4.5,
+        isProMotion: true,
+        quantity: 3,
+        totalExpense: 9
+      }
+    }
+    expect(calculateDiscount(itemDic)[0]).toEqual(58.5)
+    expect(calculateDiscount(itemDic)[1]).toEqual(7.5)
   })
 })
