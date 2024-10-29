f30f431ea0d876b230ace19c077855bb985c414e
kgaohahaha <15296612347@163.com>
2024-10-29 18:07:13 +0800

    feat: define interface

--- src/PrintReceipt.ts
+++ src/PrintReceipt.ts
@@ -1,4 +1,30 @@
-import {loadAllItems, loadPromotions} from './Dependencies'
+import { loadAllItems, loadPromotions } from './Dependencies'
+
+interface ItemRecord {
+  barcode: string;
+  name: string;
+  unit: string;
+  price: number;
+  isProMotion: boolean;
+  quantity: number;
+}
+
+interface ItemDictionary {
+  [barcode: string]: ItemRecord;
+}
+
+function convertToDictionary(items: ItemRecord[]): ItemDictionary {
+  const dictionary: ItemDictionary = {};
+  for (const item of items) {
+    if (dictionary[item.barcode]) {
+      dictionary[item.barcode].quantity += 1;
+    } else {
+      dictionary[item.barcode] = { ...item, quantity: 1 };
+    }
+  }
+  return dictionary;
+}
+
 
 export function printReceipt(tags: string[]): string {
   return ''
